2012f9a0400b282488ccab7b7a22f2f033251099
Sridhar Ratnakumar <github@srid.name>
2014-09-17 12:10:59 -0700

    suppress displaying container output
    fixes #1
    
    we channel container output using logspout already.

--- dockron.go
+++ dockron.go
@@ -47,8 +47,9 @@ func (c *Cron) Stop() {
 func execute(command string, args []string) error {
 	cmd := exec.Command(command, args...)
 
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	// Only needed to debug issues. Ideally use 'docker run -d'.
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
 
 	return cmd.Run()
 }
